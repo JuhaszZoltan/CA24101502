a672e0c3e6a8dd058464d2ed050cfabad676fdeb
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-10-15 13:21:57 +0200

    Add project files.

---
+++ Competior.cs
@@ -0,0 +1,35 @@
+﻿namespace CA24101502;
+
+internal class Competior
+{
+    public string Name { get; set; }
+    public int YearOfBirth { get; set; }
+    public string RaceNumber { get; set; }
+    public bool Gender { get; set; }
+    public string AgeCategory { get; set; }
+    public Dictionary<string, TimeSpan> RaceTimes { get; set; }
+
+    public int TotalTimeInSeconds => (int)RaceTimes.Values.Sum(ts => ts.TotalSeconds);
+
+    public override string ToString() =>
+        $"[{RaceNumber}] {Name} ({(Gender ? "férfi" : "nő")} {AgeCategory})";
+
+    public Competior(string row)
+    {
+        var v = row.Split(';');
+
+        Name = v[0];
+        YearOfBirth = int.Parse(v[1]);
+        RaceNumber = v[2];
+        Gender = v[3] == "f";
+        AgeCategory = v[4];
+        RaceTimes = new()
+        {
+            { "swimming",  TimeSpan.Parse(v[5]) },
+            { "1st depot", TimeSpan.Parse(v[6]) },
+            { "cycling",   TimeSpan.Parse(v[7]) },
+            { "2nd depot", TimeSpan.Parse(v[8]) },
+            { "running",   TimeSpan.Parse(v[9]) },
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CA24101502;
+using System.Text;
+
+List<Competior> competiors = [];
+
+using StreamReader sr = new("..\\..\\..\\src\\forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream) competiors.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"versenyt befejezok szama: {competiors.Count}");
+
+//első célba érkező férfi
+var f01 = competiors
+    .Where(c => c.Gender)
+    .MinBy(c => c.TotalTimeInSeconds);
+Console.WriteLine($"elso celba ero ferfi: {f01}");
+
+//korkategóriánként az átlagos depóban töltött
+//idő átlagidő szerint növekvő sorrendben
+var f02 = competiors
+    .GroupBy(c => c.AgeCategory)
+    .ToDictionary(
+        g => g.Key,
+        g => g
+        .Average(c =>
+        (c.RaceTimes["1st depot"] + 
+        c.RaceTimes["2nd depot"]).TotalSeconds) / 2)
+    .OrderBy(kvp => kvp.Value);
+
+Console.WriteLine("kategoriankent az atlagos depoban toltott ido:");
+foreach (var kvp in f02)
+    Console.WriteLine($"\t{kvp.Key,11}: {kvp.Value,6:0.00} sec.");
+
+
+
+
